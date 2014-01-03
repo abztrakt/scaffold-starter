d0aaf9016ba4aad34594bfcc7fa59a3599745a94
Craig M. Stimmel <cstimmel@uw.edu>
2014-01-03 13:34:00 -0800

    Put the app in the project.

--- starter.sh
+++ starter.sh
@@ -16,6 +16,7 @@ npm install less
 
 #TODO: make sure there are no hyphens in PROJECT_NAME or APP_NAME
 django-admin.py startproject --template=https://github.com/charlon/django-adaptive-proj-template/archive/master.zip --extension=py,in,html $PROJECT_NAME
+cd $PROJECT_NAME
 django-admin.py startapp --template=https://github.com/charlon/django-adaptive-app-template/archive/master.zip --extension=py,in,html $APP_NAME
 
 #TODO: add app to INSTALLED_APPS and project urls.py
