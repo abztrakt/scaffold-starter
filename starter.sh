2ef927bfffad7435495a71875d05e5254f074439
Craig M. Stimmel <cstimmel@uw.edu>
2014-01-03 13:38:39 -0800

    Install project requirements.

--- starter.sh
+++ starter.sh
@@ -1,4 +1,4 @@
-!# /bin/bash
+#!/bin/bash
 
 #TODO: prompt the user for this
 PROJECT_NAME='mytestproject'
@@ -18,5 +18,7 @@ npm install less
 django-admin.py startproject --template=https://github.com/charlon/django-adaptive-proj-template/archive/master.zip --extension=py,in,html $PROJECT_NAME
 cd $PROJECT_NAME
 django-admin.py startapp --template=https://github.com/charlon/django-adaptive-app-template/archive/master.zip --extension=py,in,html $APP_NAME
+
+pip install -r requirements.txt
 
 #TODO: add app to INSTALLED_APPS and project urls.py
