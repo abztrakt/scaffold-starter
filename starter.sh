695aa07a156c1038acdd71af3c836e2a93cf913a
Craig M. Stimmel <cstimmel@uw.edu>
2014-01-03 15:31:17 -0800

    Generate a SECRET_KEY for the settings.

--- starter.sh
+++ starter.sh
@@ -10,6 +10,7 @@ virtualenv --no-site-packages $PROJECT_NAME
 cd $PROJECT_NAME
 source bin/activate
 pip install django
+#TODO: check for lessc and give the user the ability to opt out of nodeenv?
 pip install nodeenv
 nodeenv -p
 npm install less
@@ -22,9 +23,10 @@ django-admin.py startapp --template=https://github.com/charlon/django-adaptive-a
 pip install -r requirements.txt
 
 # add app to INSTALLED_APPS and project urls.py
-#TODO: set a SECRET_KEY that is unique
 cd $PROJECT_NAME
+SECRET_KEY=`python -c 'import random; print "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'`
 sed -i.bak "s/# 'app_name'/'$APP_NAME'/g" settings.py
+sed -i.bak "s/SECRET_KEY = ''/SECRET_KEY ='$SECRET_KEY'/g" settings.py
 rm settings.py.bak
 sed -i.bak "s/#url(r'^', include('app_name.urls'))/url(r'^', include('$APP_NAME.urls'))/g" urls.py
 rm urls.py.bak
